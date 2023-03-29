c1baf682227fd659438f746ef98a8e6b59331258
danijeljw-rpc <danijel@repasscloud.com>
2023-03-30 01:00:26 +1100

    update method for docker to clean

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ scp -o "StrictHostKeyChecking=no" -rp "$SCRIPT_DIR/lib/data" "$SSH_USER@$SSH_URL
 scp -o "StrictHostKeyChecking=no" -rp "$SCRIPT_DIR/lib/home/$SSH_USER/" "$SSH_USER@$SSH_URL:/home/$SSH_USER/"
 ssh -o "StrictHostKeyChecking=no" "$SSH_USER@$SSH_URL" 'find /data -name "*.gitkeep" -type f -print0 | xargs -0 /bin/rm -f'
 
-ssh -o "StrictHostKeyChecking=no" "$SSH_USER@$SSH_URL" 'docker compose down; docker network prune --force; docker network create web; docker network create --internal internal; docker rmi $(docker images -aq) --force; docker compose up -d'
+ssh -o "StrictHostKeyChecking=no" "$SSH_USER@$SSH_URL" 'docker compose down; docker network prune --force; docker system prune -a --force; docker network create web; docker network create --internal internal; docker rmi $(docker images -aq) --force; docker compose up -d'
