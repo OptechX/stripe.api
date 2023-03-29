8b0a2d3c732f2189aae89c5d16beb3565e305b64
danijeljw-rpc <danijel@repasscloud.com>
2023-03-30 00:59:32 +1100

    update image versions

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+ssh -o "StrictHostKeyChecking=no" "$SSH_USER@$SSH_URL" 'rm -rf docker-compose.yml'
+
 scp -o "StrictHostKeyChecking=no" -rp "$SCRIPT_DIR/lib/data" "$SSH_USER@$SSH_URL:/"
 scp -o "StrictHostKeyChecking=no" -rp "$SCRIPT_DIR/lib/home/$SSH_USER/" "$SSH_USER@$SSH_URL:/home/$SSH_USER/"
 ssh -o "StrictHostKeyChecking=no" "$SSH_USER@$SSH_URL" 'find /data -name "*.gitkeep" -type f -print0 | xargs -0 /bin/rm -f'
