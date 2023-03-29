8ee7897f27028c2b5bc2efdf7c0a9f9d761f0aa1
danijeljw-rpc <danijel@repasscloud.com>
2023-03-30 01:04:58 +1100

    update image versions

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ssh -o "StrictHostKeyChecking=no" "$SSH_USER@$SSH_URL" 'rm -rf docker-compose.yml'
 
 scp -o "StrictHostKeyChecking=no" -rp "$SCRIPT_DIR/lib/data" "$SSH_USER@$SSH_URL:/"
-scp -o "StrictHostKeyChecking=no" -rp "$SCRIPT_DIR/lib/home/$SSH_USER/" "$SSH_USER@$SSH_URL:/home/$SSH_USER/"
+scp -o "StrictHostKeyChecking=no" -rp "$SCRIPT_DIR/lib/home/$SSH_USER/docker-compose.yml" "$SSH_USER@$SSH_URL:/home/$SSH_USER/docker-compose.yml"
 ssh -o "StrictHostKeyChecking=no" "$SSH_USER@$SSH_URL" 'find /data -name "*.gitkeep" -type f -print0 | xargs -0 /bin/rm -f'
 
 ssh -o "StrictHostKeyChecking=no" "$SSH_USER@$SSH_URL" 'docker compose down; docker network prune --force; docker system prune -a --force; docker network create web; docker network create --internal internal; docker compose up -d'
