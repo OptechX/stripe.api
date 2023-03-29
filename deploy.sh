8c159c42e031fc8ae8ce577ccaf549c462fa78ec
danijeljw-rpc <danijel@repasscloud.com>
2023-03-30 00:54:42 +1100

    encrypt publish

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 scp -o "StrictHostKeyChecking=no" -rp "$SCRIPT_DIR/lib/data" "$SSH_USER@$SSH_URL:/"
 scp -o "StrictHostKeyChecking=no" -rp "$SCRIPT_DIR/lib/home/$SSH_USER/" "$SSH_USER@$SSH_URL:/home/$SSH_USER/"
 ssh -o "StrictHostKeyChecking=no" "$SSH_USER@$SSH_URL" 'find /data -name "*.gitkeep" -type f -print0 | xargs -0 /bin/rm -f'
+
+ssh -o "StrictHostKeyChecking=no" "$SSH_USER@$SSH_URL" 'docker compose down; docker network prune --force; docker network create web; docker network create --internal internal; docker rmi $(docker images -aq) --force; docker compose up -d'
